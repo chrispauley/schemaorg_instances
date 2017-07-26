d59eacc772a22292f00fc2ee5e95127ddfc6fd53
chrispauley <pauleyc@yahoo.com>
2017-07-26 14:23:15 -0400

    Adds initial source files

---
+++ src/js/immediate-f.js
@@ -0,0 +1,17 @@
+(function(){
+  var data = {
+    "@contect": "http://schema.org",
+    "@type": "Organization",
+    "name": "SERPs.com",
+    "url": "http://serps.com",
+    "logo": "https://serps.com/app/blah/2016/05/serps-logo-60.png",
+    "sameAs": [
+      "https://www.facebook.com/serpsapp",
+      "https://twitter.com/serpsapp"
+    ],
+    var script = document.createElement('script');
+    script.type = "application/ld+json";
+    script.innerHTML = JSON.stringify(data);
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
+})(document);
